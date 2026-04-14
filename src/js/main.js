808ddb1b037d898d15b4ba344c24dc29f52d8f0d
Mariana Iñiguez <meimc93@gmail.com>
2026-04-14 17:45:28 -0500

    CV, descripción, proyectos y titulo de contacto en HTML y JS para header y footer

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-console.log("JS cargado");
-const boton = document.getElementById("menu-equipo");
-const menu = document.getElementById("dropdown-menu");
-
-boton.addEventListener("click", function(e){
-    e.preventDefault();
-    menu.classList.toggle("active");
-});
